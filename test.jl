775298dc54994d9172c36975d79e2585cbb3a69c
p-stokes <patrick.stokes@gmail.com>
2023-10-16 10:25:44 -0400

    Filled in multisplit_generators and ens_sub_split, and modified split2 and bindunbind_multisite.

--- test.jl
+++ test.jl
@@ -4,15 +4,43 @@ function split(cat,prod1,prod2,on::T) where T
   Open(LabelledReactionNet{T,Number}(unique((in=>0, cat,prod1,prod2)), ((Symbol(:split_,in),on),in=>(first(cat),first(prod1),first(prod2)))));
 end;
 
+function split(cat,prod1,prod2)
+  in = Symbol(cat, sep_sym,prod1, sep_sym,prod2)
+  Open(LabelledPetriNet(unique((in, cat,prod1,prod2)), (Symbol(:split_,in),in=>(cat,prod1,prod2))));
+end;
+
+function split2(cat,sub,site::Int,on::T) where T
+  catsym = first(cat)
+  subsym = first(sub)
+  in = Symbol(catsym,site,sep_sym,subsym)
+  prod1 = Symbol(String(subsym[1:site]))
+  prod2 = Symbol(String(subsym[site+1:end]))
+  Open(LabelledReactionNet{T,Number}(unique((in=>0, cat,prod1=>0,prod2=>0)), ((Symbol(:split_,in),on),in=>(first(cat),prod1,prod2))));
+end;
+
+function split2(cat,sub,site::Int)
+  in = Symbol(cat,site,sep_sym,sub)
+  prod1 = Symbol(String(sub[1:site]))
+  prod2 = Symbol(String(sub[site+1:end]))
+  Open(LabelledPetriNet(unique((in, cat,prod1,prod2)), (Symbol(:split_,in),in=>(cat,prod1,prod2))));
+end;
+
 # Beginning of a multisplit motif. Not sure if needed.
 function multisplit(enzs,prods,on::T) where T
   in = Symbol(first(cat), sep_sym,first(prod1), sep_sym,first(prod2))
   Open(LabelledReactionNet{T,Number}(unique((in=>0, cat,prod1,prod2)), ((Symbol(:split_,in),on),in=>(first(cat),first(prod1),first(prod2)))));
 end;
 
-function split(cat,prod1,prod2)
-  in = Symbol(cat, sep_sym,prod1, sep_sym,prod2)
-  Open(LabelledPetriNet(unique((in, cat,prod1,prod2)), (Symbol(:split_,in),in=>(cat,prod1,prod2))));
+function bindunbind_multisite(in1, in2, site::Int, on::T, off::T) where T
+  out = Symbol(first(in1),site, sep_sym,first(in2))
+  Open(LabelledReactionNet{T,Number}(unique((in1, in2, out=>0)), ((Symbol(:bind,site,_,first(in1), sep_sym,first(in2)),on),(first(in1),first(in2))=>out),
+    ((Symbol(:unbind_,out),off),out=>(first(in1),first(in2)))))
+end;
+
+function bindunbind_multisite(in1, in2, site::Int)
+  out = Symbol(in1,site, sep_sym,in2)
+  Open(LabelledPetriNet(unique((in1, in2,out)), (Symbol(:bind,site,_,in1, sep_sym,in2),(in1,in2)=>out),
+                                                (Symbol(:unbind_,out),out=>(in1,in2))))
 end;
 
 enzXsubYZ = @relation (X, Xinact, Xdeg, YZ, Y, Z) where (X, Xinact, Xdeg, YZ, XYZ, Y, Z) begin
@@ -60,19 +88,56 @@ function enz_sub2(cat1::Symbol, sub1::Symbol, sub2::Symbol)
   bundle_legs(out, [[1,2,3], [4,5]])
 end
 
-# Just old copy at moment. Not sure if needed. Could be used to set up multisplitting
-function enzyme_generators(enzymes::Array{Symbol}, substrates::Array{Symbol})
+function enz_sub_split(rxns, cat1, sub, site, offset=0)
+  catsym = first(cat1)
+  subsym = first(sub)
+
+  catsub = Symbol(catsym, site+offset, sep_sym, subsym)
+  frag1sym = Symbol(String(subsym)[1:site])
+  frag2sym = Symbol(String(subsym)[site+1:end])
+    
+  obtype = valtype(rates(apex(first(last(first(rxns))))))
+  out = oapply(enzXsubYZ, Dict([:bindXYZ, :splitXYZ] .=> rxns[catsub]), Dict(
+    :X=>ob(obtype, cat1),
+    :Xinact=>ob(obtype, Symbol(catsym,:_inact)=>0),
+    :Xdeg=>ob(obtype, Symbol(catsym,:_deg)=>0),
+    :YZ=>ob(obtype, subsym),
+    :XYZ=>ob(obtype, catsub=>0),
+    :Y=>ob(obtype, frag1sym=>0),
+    :Z=>ob(obtype, frag2sym=>0)))
+  bundle_legs(out, [[1,2,3], [4,5]])
+end
+
+function enz_sub_split(cat1::Symbol, sub::Symbol, site, offset)
+  catsym = cat1
+  subsym = sub
+
+  catsub = Symbol(catsym, site+offset, sep_sym, subsym)
+  frag1sym = Symbol(String(subsym)[1:site])
+  frag2sym = Symbol(String(subsym)[site+1:end])
+
+  out = oapply(enzXsubYZ, Dict(:bindXYZ=>bindunbind_multisite(catsym,subsym,site), :splitXYZ=>split2(catsym,subsym,site)), Dict(
+    :X=>ob(cat1),
+    :Xinact=>ob(Symbol(catsym,:_inact)),
+    :Xdeg=>ob(Symbol(catsym,:_deg)),
+    :YZ=>ob(subsym),
+    :XYZ=>ob(catsub),
+    :Y=>ob(frag1sym),
+    :Z=>ob(frag2sym)))
+  bundle_legs(out, [[1,2,3], [4,5]])
+end
+
+function multisplit_generators(enzyme::Symbol, molecule::Symbol, offset::Int)
   gens = Dict{Symbol, Any}()
-  for e1 in enzymes
-    for e2 in enzymes
-      if e1 == e2
-        gens[Symbol(:cat, e1)] = enz(e1)
-      else
-        gens[Symbol(:cat, e1, :cat, e2)] = enz_enz(e1, e2)
-      end
+  for ii in 1:(length(String(molecule))-1)
+    gens[Symbol(:cat, enzyme, offset+ii, sep_sym :sub, molecule)] = enz_sub_split(enzyme, molecule, ii, offset)  
+    frag1 = Symbol(String(molecule)[1:ii])
+    frag2 = Symbol(String(molecule)[ii+1:end])
+    if ii != 1
+      merge!(gens,multisplit_generators(enzyme, frag1, offset))
     end
-    for s in substrates
-      gens[Symbol(:cat, e1, :sub, s)] = enz_sub(e1, s)
+    if ii != length(String(molecule))-1
+      merge!(gens,multisplit_generators(enzyme, frag2, offset+ii))
     end
   end
   gens
