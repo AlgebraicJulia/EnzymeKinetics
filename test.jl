9953555b74c01abdccc64e8985d72cd79cbe8725
p-stokes <patrick.stokes@gmail.com>
2023-10-17 01:48:14 -0400

    Commented old split functions. Removed site offsets. Fixed bug in multisplit_generators.

--- test.jl
+++ test.jl
@@ -1,27 +1,11 @@
-
-function split(cat,prod1,prod2,on::T) where T
+#=
+function split1(cat,prod1,prod2,on::T) where T
   in = Symbol(first(cat), sep_sym,first(prod1), sep_sym,first(prod2))
   Open(LabelledReactionNet{T,Number}(unique((in=>0, cat,prod1,prod2)), ((Symbol(:split_,in),on),in=>(first(cat),first(prod1),first(prod2)))));
 end;
 
-function split(cat,prod1,prod2)
+function split1(cat,prod1,prod2)
   in = Symbol(cat, sep_sym,prod1, sep_sym,prod2)
-  Open(LabelledPetriNet(unique((in, cat,prod1,prod2)), (Symbol(:split_,in),in=>(cat,prod1,prod2))));
-end;
-
-function split2(cat,sub,site::Int,on::T) where T
-  catsym = first(cat)
-  subsym = first(sub)
-  in = Symbol(catsym,site,sep_sym,subsym)
-  prod1 = Symbol(String(subsym[1:site]))
-  prod2 = Symbol(String(subsym[site+1:end]))
-  Open(LabelledReactionNet{T,Number}(unique((in=>0, cat,prod1=>0,prod2=>0)), ((Symbol(:split_,in),on),in=>(first(cat),prod1,prod2))));
-end;
-
-function split2(cat,sub,site::Int)
-  in = Symbol(cat,site,sep_sym,sub)
-  prod1 = Symbol(String(sub[1:site]))
-  prod2 = Symbol(String(sub[site+1:end]))
   Open(LabelledPetriNet(unique((in, cat,prod1,prod2)), (Symbol(:split_,in),in=>(cat,prod1,prod2))));
 end;
 
@@ -31,22 +15,6 @@ function multisplit(enzs,prods,on::T) where T
   Open(LabelledReactionNet{T,Number}(unique((in=>0, cat,prod1,prod2)), ((Symbol(:split_,in),on),in=>(first(cat),first(prod1),first(prod2)))));
 end;
 
-function bindunbind_multisite(in1, in2, site::Int, on::T, off::T) where T
-  out = Symbol(first(in1),site, sep_sym,first(in2))
-  Open(LabelledReactionNet{T,Number}(unique((in1, in2, out=>0)), ((Symbol(:bind,site,_,first(in1), sep_sym,first(in2)),on),(first(in1),first(in2))=>out),
-    ((Symbol(:unbind_,out),off),out=>(first(in1),first(in2)))))
-end;
-
-function bindunbind_multisite(in1, in2, site::Int)
-  out = Symbol(in1,site, sep_sym,in2)
-  Open(LabelledPetriNet(unique((in1, in2,out)), (Symbol(:bind,site,_,in1, sep_sym,in2),(in1,in2)=>out),
-                                                (Symbol(:unbind_,out),out=>(in1,in2))))
-end;
-
-enzXsubYZ = @relation (X, Xinact, Xdeg, YZ, Y, Z) where (X, Xinact, Xdeg, YZ, XYZ, Y, Z) begin
-  bindXYZ(X, YZ, XYZ)
-  splitXYZ(XYZ, X, Y, Z)
-end
 
 # TODO:
 # 1. Currently assumes the structures/concentrations are initialize for the end products,
@@ -87,12 +55,47 @@ function enz_sub2(cat1::Symbol, sub1::Symbol, sub2::Symbol)
     :Z=>ob(sub2)))
   bundle_legs(out, [[1,2,3], [4,5]])
 end
+=#
 
-function enz_sub_split(rxns, cat1, sub, site, offset=0)
+function split2(cat,sub,site::Int,on::T) where T
+  catsym = first(cat)
+  subsym = first(sub)
+  in = Symbol(catsym,site,sep_sym,subsym)
+  prod1 = Symbol(String(subsym)[1:site])
+  prod2 = Symbol(String(subsym)[site+1:end])
+  Open(LabelledReactionNet{T,Number}(unique((in=>0, cat,prod1=>0,prod2=>0)), ((Symbol(:split_,in),on),in=>(first(cat),prod1,prod2))));
+end;
+
+function split2(cat,sub,site::Int)
+  in = Symbol(cat,site,sep_sym,sub)
+  prod1 = Symbol(String(sub)[1:site])
+  prod2 = Symbol(String(sub)[site+1:end])
+  Open(LabelledPetriNet(unique((in, cat,prod1,prod2)), (Symbol(:split_,in),in=>(cat,prod1,prod2))));
+end;
+
+function bindunbind_multisite(in1, in2, site::Int, on::T, off::T) where T
+  out = Symbol(first(in1),site, sep_sym,first(in2))
+  Open(LabelledReactionNet{T,Number}(unique((in1, in2, out=>0)), ((Symbol(:bind,site,:_,first(in1), sep_sym,first(in2)),on),(first(in1),first(in2))=>out),
+    ((Symbol(:unbind_,out),off),out=>(first(in1),first(in2)))))
+end;
+
+function bindunbind_multisite(in1, in2, site::Int)
+  out = Symbol(in1,site, sep_sym,in2)
+  Open(LabelledPetriNet(unique((in1, in2,out)), (Symbol(:bind,site,:_,in1, sep_sym,in2),(in1,in2)=>out),
+                                                (Symbol(:unbind_,out),out=>(in1,in2))))
+end;
+
+enzXsubYZ = @relation (X, Xinact, Xdeg, YZ, Y, Z) where (X, Xinact, Xdeg, YZ, XYZ, Y, Z) begin
+  bindXYZ(X, YZ, XYZ)
+  splitXYZ(XYZ, X, Y, Z)
+end
+
+
+function enz_sub_split(rxns, cat1, sub, site)
   catsym = first(cat1)
   subsym = first(sub)
 
-  catsub = Symbol(catsym, site+offset, sep_sym, subsym)
+  catsub = Symbol(catsym, site, sep_sym, subsym)
   frag1sym = Symbol(String(subsym)[1:site])
   frag2sym = Symbol(String(subsym)[site+1:end])
     
@@ -108,11 +111,11 @@ function enz_sub_split(rxns, cat1, sub, site, offset=0)
   bundle_legs(out, [[1,2,3], [4,5]])
 end
 
-function enz_sub_split(cat1::Symbol, sub::Symbol, site, offset)
+function enz_sub_split(cat1::Symbol, sub::Symbol, site)
   catsym = cat1
   subsym = sub
 
-  catsub = Symbol(catsym, site+offset, sep_sym, subsym)
+  catsub = Symbol(catsym, site, sep_sym, subsym)
   frag1sym = Symbol(String(subsym)[1:site])
   frag2sym = Symbol(String(subsym)[site+1:end])
 
@@ -127,18 +130,53 @@ function enz_sub_split(cat1::Symbol, sub::Symbol, site, offset)
   bundle_legs(out, [[1,2,3], [4,5]])
 end
 
-function multisplit_generators(enzyme::Symbol, molecule::Symbol, offset::Int)
+function multisplit_generators(enzyme::Symbol, molecule::Symbol)
   gens = Dict{Symbol, Any}()
   for ii in 1:(length(String(molecule))-1)
-    gens[Symbol(:cat, enzyme, offset+ii, sep_sym :sub, molecule)] = enz_sub_split(enzyme, molecule, ii, offset)  
+    gens[Symbol(:cat, enzyme, ii, sep_sym, :sub, molecule)] = enz_sub_split(enzyme, molecule, ii)  
     frag1 = Symbol(String(molecule)[1:ii])
     frag2 = Symbol(String(molecule)[ii+1:end])
     if ii != 1
-      merge!(gens,multisplit_generators(enzyme, frag1, offset))
+      merge!(gens,multisplit_generators(enzyme, frag1))
     end
     if ii != length(String(molecule))-1
-      merge!(gens,multisplit_generators(enzyme, frag2, offset+ii))
+      merge!(gens,multisplit_generators(enzyme, frag2))
     end
   end
   gens
 end
+
+# CURRENTLY JUST A COPY OF enzyme_uwd
+function multisplit_uwd(enzymes::Array{Symbol}, substrates::Array{Symbol})
+  rel = RelationDiagram{Symbol}(0)
+
+  chemicals = vcat(substrates, enzymes)
+
+  subs = add_junctions!(rel, length(substrates), variable=substrates)
+  enzs = add_junctions!(rel, length(enzymes), variable=enzymes)
+  nsubs = length(subs)
+  nenzs = length(enzs)
+
+  catx = add_parts!(rel, :Box, nenzs, name=[Symbol("cat$i") for i in enzymes])
+  add_parts!(rel, :Port, nenzs, junction=enzs, box=catx)
+
+  for x in 1:nenzs
+    for y in 1:nenzs
+      if y != x
+        catxy = add_part!(rel, :Box, name=Symbol("cat$(enzymes[x])cat$(enzymes[y])"))
+        add_parts!(rel, :Port, 2, junction=[enzs[x], enzs[y]], box=catxy)
+      end
+    end
+  end
+
+  for x in 1:nenzs
+    for y in 1:nsubs
+      catxy = add_part!(rel, :Box, name=Symbol("cat$(enzymes[x])sub$(substrates[y])"))
+      add_parts!(rel, :Port, 2, junction=[enzs[x], subs[y]], box=catxy)
+    end
+  end
+  add_parts!(rel, :OuterPort, length(chemicals), outer_junction = vcat(subs, enzs))
+  rel
+end
+
+
